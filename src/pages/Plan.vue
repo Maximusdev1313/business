923e8b5d415fc5300ef9218373cab605f14dc4c5
maximusdev1313 <maximusdev1313@gmail.com>
2022-08-30 13:23:44 +0500

    tested mixins with inputs

--- src/pages/Plan.vue
+++ src/pages/Plan.vue
@@ -3,15 +3,47 @@
     <CompWorks />
     <div class="w-100pr column justify-center content-center">
       <div class="w-90pr q-ma-md">
+        <!-- <div class="flex justify-between">
+          <q-input
+            v-model="name"
+            label="Ismingiz"
+            class="w-40pr"
+            placeholder="To'liq ismingiz"
+            type="text"
+            :rules="[(val) => !!val || '* Iltimos ismingizni kiriting']"
+          />
+          <q-input
+            v-model="name"
+            label="Familyangiz"
+            class="w-40pr"
+            type="text"
+            :rules="[(val) => !!val || '* Iltimos familyangizni kiriting']"
+          />
+        </div> -->
         <q-input
           v-model="name"
-          label="Ismingiz"
-          placeholder="To'liq ismingiz"
+          label="Kompaniya nomi"
           type="text"
-          :rules="[
-            (val) => !!val || '* Iltimos ismingizni kiriting',
-          ]"
+          :rules="[(val) => !!val || '* Iltimos kompaniya nomini kiriting']"
         />
+        <q-input
+          v-model="creditSize"
+          label="Kredit miqdori"
+          type="number"
+        />
+         <q-input
+          v-model="month"
+          label="necha oyga"
+          type="number"
+        />
+         <q-input
+          v-model="persent"
+          label="Foizi"
+          type="number"
+        />
+        <q-btn @click="totalCredit">
+          Hisoblash
+        </q-btn>
       </div>
     </div>
   </div>
@@ -20,16 +52,23 @@
 <script>
 import CompWorks from "src/components/forMainPage/CompWorks";
 import { ref } from "vue";
+import counterForCredit from '../mixins/counterForCredit'
 export default {
   components: {
     CompWorks,
   },
   setup() {
-    const name = ref("");
-    console.log(name.value);
+    const name = ref('')
+    
+    const {creditSize, month, persent, totalPersent,totalCredit } = counterForCredit()
 
     return {
-      name,
+      creditSize,
+      month,
+      persent,
+      totalPersent,
+      totalCredit,
+      name
     };
   },
 };
